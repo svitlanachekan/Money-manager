e37eec9e80b945e18774420cbed260d84f7d2ee7
svitlanachekan <spy1374@gmail.com>
2025-11-10 19:33:16 +0100

    changes

--- main.py
+++ main.py
@@ -15,10 +15,47 @@
 # Введіть ціну за одиницю: 25
 # ✅ Хліб додано до списку!
 
+def add_item(shopping_list):
+    name = input("Enter a name: ")
+    quantity = int(input("Enter a quantity: "))
+    price = float(input("Enter a price: "))
+    
+    item = { #cписок словників, у яких різні ключі
+        "name": name,
+        "quantity": quantity,
+        "price": price
+
+    }
+
+    shopping_list.append(item)
+
+    print(f"✅ {name} is added to list!") # "✅" + "name" is added to list!
+
+def show_list(shopping_list):
+    # shopping_list = [{"name": "Bread", "quantity": 2, "price"; 2.0}]
+    # for i in range(len(shopping_list)):
+    #     print(f"{i+1}. {shopping_list[i]["name"]} - {shopping_list[i]["quantity"]} x {shopping_list[i]["price"]}€") # зверниться до словника і поверне Хліб
+
+ if not shopping_list:
+        print("List is empty")
+        return
+    
+    print("\nYour list: ")
+
+def count_total():
+    pass
+
+def save_to_file():
+    pass
+
+def load_from_file():
+    pass
+                
+
 
 def main():
     print("Вітаю у менеджері покупок!")
-    shopping_list= []
+    shopping_list = []
 
     while True:
         print('''
@@ -30,17 +67,37 @@ def main():
     5. Завантажити з файлу
     6. Вихід
             ''')
-        
+        try:
+            choice = int(input("Enter a buying: "))
+        except ValueError:
+            print("Enter number 1-6!!")
 
-        choice = input("Enter a buying: ")
+        # if choice == 1: #иконується тільки одна із умов
+        #     pass
+        # elif choice == 2:
+        #     pass
+
+        match choice:
+            case 1: # необхідно зупиняти
+                add_item(shopping_list)
+            case 2:
+                show_list(shopping_list)
+            case 3:
+                count_total()
+            case 4:
+                save_to_file()
+            case 5:
+                load_from_file()
+            case 6:
+                print("See you!")
+                break
+            case _: # не знайшло попереднє, виконай це
+                pass
+
+
 
 
 main()
 
 
 
-# buying = str(input("Enter a buying: "))
-
-
-
-# print(list)
